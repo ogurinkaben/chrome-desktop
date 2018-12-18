64b4ce2f11c29af88a121f2645815561e2f3478e
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2018-12-18 18:08:07 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function check() {
+    var today = new Date();
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + "." + m ;
+      document.getElementById("date").innerHTML=today.toLocaleDateString("en-US", options);
+    t = setInterval(function() {
+      check()
+    }, 60000);
+  }
+  
+  check();
+  var time = new Date();
+  var ch = time.getHours();
+  if (ch >= 18 && ch <=23) {
+    document.getElementById("st").innerHTML="Having a sublime night?";
+  }
+  else if (ch >= 12 && ch < 18) {
+      document.getElementById("st").innerHTML="Having a good day?";
+  } 
+  else {
+    document.getElementById("st").innerHTML="Good Morning";
+  }
