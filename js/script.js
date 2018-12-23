a47fff368b7283fbb4495ce2e727b5ef63ad2098
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2018-12-23 21:01:02 +0100

    Update via upload

--- js/script.js
+++ js/script.js
@@ -31,4 +31,21 @@ function checkTime(i) {
   } 
   else {
     document.getElementById("st").innerHTML="Good Morning";
-  }
+}
+
+//Quote Generator  
+var quote = document.getElementById('quote');
+var author = document.getElementById('author');
+
+function foo(data) {
+  quote.innerHTML = data.quoteText;
+  author.innerHTML = '- ' + data.quoteAuthor;
+}
+
+function insertQuote() {
+  var script = document.createElement('script');
+  script.src = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=foo';
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+insertQuote();
