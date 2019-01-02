776d76202bf69e95608cec6ae3754606687065bd
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-01-02 04:33:00 +0100

    Update UI

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function checkTime(i) {
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML = h + "." + m ;
+    document.getElementById('time').innerHTML = h + ":" + m ;
       document.getElementById("date").innerHTML=today.toLocaleDateString("en-US", options);
     t = setInterval(function() {
       check()
@@ -24,28 +24,11 @@ function checkTime(i) {
   var time = new Date();
   var ch = time.getHours();
   if (ch >= 18 && ch <=23) {
-    document.getElementById("st").innerHTML="Having a sublime night?";
+    document.getElementById("msg").innerHTML="Having a sublime night?";
   }
   else if (ch >= 12 && ch < 18) {
-      document.getElementById("st").innerHTML="Having a good day?";
+      document.getElementById("msg").innerHTML="Having a good day?";
   } 
   else {
-    document.getElementById("st").innerHTML="Good Morning";
+    document.getElementById("msg").innerHTML="Good Morning";
 }
-
-//Quote Generator  
-var quote = document.getElementById('quote');
-var author = document.getElementById('author');
-
-function foo(data) {
-  quote.innerHTML = data.quoteText;
-  author.innerHTML = '- ' + data.quoteAuthor;
-}
-
-function insertQuote() {
-  var script = document.createElement('script');
-  script.src = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=foo';
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-insertQuote();
