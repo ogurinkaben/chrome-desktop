a892d815f359d938d1e4b887474591d32a13968e
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2020-04-03 13:19:36 +0100

    Rewrite todo with vue (with the help of a boilerplate code

--- js/script.js
+++ js/script.js
@@ -38,107 +38,78 @@ setInterval(digitalClock, 1000);
 
 
 // Todo List
-class Note {
-  constructor(noteTitle, shortNote, year) {
-    this.noteTitle = noteTitle;
-    this.shortNote = shortNote;
-    this.year = year;
+var filters = {
+  all: function(todos) {
+    return todos;
+  },
+  complete: function(todos) {
+    return todos.filter(function(todo) {
+      return todo.complete;
+    });
+  },
+  incomplete: function(todos) {
+    return todos.filter(function(todo) {
+      return !todo.complete;
+    });
   }
 }
 
-class UI {
-  addNote(note) {
-    const
-      noteList = document.querySelector('#note-list'),
-      list = document.createElement('div');
-    list.innerHTML = `
-    <div class="ui-list">
-        <p>Title: ${note.noteTitle} </p>
-      <p>Note: ${note.shortNote} </p>
-      <p> <a href="#" class="delete"> Delete Note </a> </p>
-      </div>
-      `;
-    noteList.appendChild(list);
-  }
-
-  deleteNote(target) {
-    if (target.className === 'delete') {
-      target.parentElement.parentElement.remove();
-    } else {
-
-    }
-  }
-
-  clearFields() {
-    document.querySelector('#noteTitle').value = "";
-    document.querySelector('#shortNote').value = "";
+var STORAGE_KEY = 'vue-js-todo-P7oZi9sL'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   }
 }
 
-class Store {
-  static fetchNotes() {
-    let notes;
-    if (localStorage.getItem('notes') === null) {
-      notes = [];
-    } else {
-      notes = JSON.parse(localStorage.getItem('notes'));
-    }
-
-    return notes;
-  }
-  static displayNotes() {
-    const notes = Store.fetchNotes();
-    notes.forEach(function(note) {
-      const ui = new UI;
-
-      ui.addNote(note)
-    })
-  }
-
-  static addNotes(note) {
-    const notes = Store.fetchNotes();
-    notes.push(note);
-
-    localStorage.setItem('notes', JSON.stringify(notes));
-  }
-  static removeNote(year) {
-    const notes = Store.fetchNotes();
-    notes.forEach(function(note, index) {
-      if (note.year = year) {
-        notes.splice(index, 1)
+var app = new Vue({
+  el: '#app',
+  data: {
+    inputVal: '',
+    todos: todoStorage.fetch(),
+    visibility: 'all',
+    active: false,
+    errors: [],
+  },
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos);
       }
-    })
+    }
+  },
+  computed: {
+    filteredTodos: function() {
+      return filters[this.visibility](this.todos);
+    }
+  },
+  methods: {
+    addTodo: function(e) {
+      e.preventDefault();
+      this.errors = []
+      if (this.inputVal) {
+        this.todos.push({
+          text: this.inputVal,
+          complete: false
+        });
+      } else {
 
-    localStorage.setItem('notes', JSON.stringify(notes));
-
+        this.errors.push('Enter an item.');
+      }
+      this.inputVal = '';
+    },
+    toggleTodo: function(todo) {
+      todo.complete = !todo.complete;
+    },
+    filterTodos: function(filter) {
+      this.visibility = filter;
+      this.active = true;
+    },
+    deleteTodo: function(index) {
+      this.todos.splice(index, 1);
+    }
   }
-}
-document.addEventListener('DOMContentLoaded', Store.displayNotes);
-document.querySelector('#note-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  const
-    noteTitle = document.querySelector('#noteTitle').value,
-    shortNote = document.querySelector('#shortNote').value,
-    year = document.querySelector('#year').value,
-
-    note = new Note(noteTitle, shortNote, year),
-    ui = new UI();
-
-  if (noteTitle === '' || shortNote === '' || year === '') {
-    alert("Fill all Fields")
-  } else {
-    ui.addNote(note);
-
-    Store.addNotes(note);
-    ui.clearFields();
-  }
-
 });
-
-document.getElementById('note-list').addEventListener('click', function(e) {
-  const ui = new UI();
-  ui.deleteNote(e.target);
-  Store.removeNote(e.target.parentElement.previousElementSibling.textContent);
-  e.preventDefault();
-})
